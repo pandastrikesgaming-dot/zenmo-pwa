55f1f62998ae7592885c27c5718f64b2c0e80dad
pandastrikesgaming-dot <pandastrikesgaming@gmail.com>
2026-05-03 02:26:27 +0530

    Fix PWA installation: update manifest config and cache onboarding route

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'zenmo-pwa-v1';
-const APP_SHELL = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE_NAME = 'zenmo-pwa-v2';
+const APP_SHELL = ['/', '/onboarding', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
